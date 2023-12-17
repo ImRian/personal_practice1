beb99e701a11e34ce1ff2c31c32f3df4ad007d49
임리안 <z1aaannsss@gmail.com>
2023-12-18 05:48:38 +0900

    스캘레톤 코드 까지 업데이트

--- skeleton/components/Calendar.jsx
+++ skeleton/components/Calendar.jsx
@@ -1,52 +1,62 @@
 // import React from 'react';
 // import moment from 'moment';
+// import '../styles/Calendar.css'; // CSS 파일 임포트
 
 // const Calendar = ({ currentWeek, routines, onSelectDay, onNextWeek, onPreviousWeek, onRoutineStatusChange, onRoutineEdit }) => {
 
-//   const Calendar = ({ currentWeek, routines, onSelectDay, onNextWeek, onPreviousWeek, onRoutineStatusChange, onRoutineEdit }) => {
-//     const renderWeekTitle = () => {
-//       if (currentWeek.length === 0) {
-//         return '로딩 중...';
-//       }
-  
-//       const start = currentWeek[0];
-//       const end = currentWeek[currentWeek.length - 1];
-//       return `${start.format('YYYY-MM-DD')} ~ ${end.format('MM-DD')} 주간`;
-//     };
-  
-//     const currentWeekKey = currentWeek[0]?.format('YYYY-MM-DD');
-
-
-
-//   // TODO: 주간별 루틴을 렌더링하는 함수를 작성하세요.
-//   const renderRoutinesForDay = (day) => {
-//     // 이 부분을 채워 넣으시면 됩니다.
+//   // 주간 타이틀을 렌더링하는 함수입니다
+//   const renderWeekTitle = () => {
+//     if (currentWeek.length === 0) {
+//       return '로딩 중...';
+//     }
+//     // TODO: 현재 주간을 표시하는 로직 구현해 보세요
+//     // 이 공간을 채워 넣으시면 됩니다
 //   };
 
-//   return (
-//     <div>
-//       {/* 주간 타이틀과 변경 버튼을 렌더링합니다. */}
-//       <div>
-//         <button onClick={onPreviousWeek}>&lt;</button>
-//         {renderWeekTitle()}
-//         <button onClick={onNextWeek}>&gt;</button>
-//       </div>
+//   // 현재 주의 키 값 계산
+//   const currentWeekKey = currentWeek[0]?.format('YYYY-MM-DD');
 
-//       {/* 주간별 루틴 테이블을 렌더링합니다. */}
-//       <table>
-//         <thead>
-//           <tr>
-//             {/* TODO: 헤더 렌더링 로직을 작성하세요. */}
-//           </tr>
-//         </thead>
-//         <tbody>
-//           <tr>
-//             {/* TODO: 각 요일별 루틴 렌더링 로직을 작성하세요. */}
-//           </tr>
-//         </tbody>
-//       </table>
+//   // 각 요일에 해당하는 루틴을 렌더링하는 함수입니다.
+//   const renderRoutinesForDay = (day) => {
+//   // 현재 주간의 키 값을 이용해 해당 요일의 루틴을 찾습니다.
+//   // TODO: 주어진 'day'에 해당하는 루틴을 'routines' 객체에서 필터링합니다.
+
+//   // 각 루틴에 대해 다음을 수행합니다:
+//   // 1. 루틴 이름을 수정할 수 있는 입력 필드를 렌더링합니다.
+//   //    사용자가 입력 필드를 변경하면 onRoutineEdit 함수가 호출되어야 합니다.
+//   // 2. 루틴의 진행 상태(예: 'O', 'X', '진행중')를 선택할 수 있는 드롭다운을 렌더링합니다.
+//   //    사용자가 선택을 변경하면 onRoutineStatusChange 함수가 호출되어야 합니다.
+//   // TODO: 각 루틴에 대한 입력 필드와 드롭다운을 렌더링하는 코드를 작성하세요.
+// };
+
+
+// return (
+//   <div>
+//     <div className="week-navigation">
+//       <button onClick={onPreviousWeek}>&lt;</button>
+//       <h2>{renderWeekTitle()}</h2>
+//       <button onClick={onNextWeek}>&gt;</button>
 //     </div>
-//   );
+//     <table>
+//       <thead>
+//         <tr>
+//           {currentWeek.map((day, index) => (
+//             <th key={index}>{day.format('ddd (MM/DD)')}</th>
+//           ))}
+//         </tr>
+//       </thead>
+//       <tbody>
+//         <tr>
+//           {currentWeek.map((day, index) => (
+//             <td key={index}>
+//               {renderRoutinesForDay(day)}
+//             </td>
+//           ))}
+//         </tr>
+//       </tbody>
+//     </table>
+//   </div>
+// );
 // };
 
 // export default Calendar;
--- skeleton/containers/RoutineManager.jsx
+++ skeleton/containers/RoutineManager.jsx
@@ -1,34 +1,101 @@
 // import React, { useState } from "react";
 // import Calendar from "../components/Calendar";
 // import useCalendar from "../hooks/useCalendar";
+// import '../styles/RoutineManager.css';
 
 // const RoutineManager = () => {
-//   // useCalendar 훅을 사용하여 현재 주간 데이터를 관리합니다.
 //   const { currentWeek, nextWeek, previousWeek } = useCalendar();
 
-//   // TODO: 루틴 데이터 상태 관리를 위한 코드를 작성하세요.
-//   // const [routines, setRoutines] = useState({});
+//   // 루틴과 관련된 상태들입니다.
+//   const [routines, setRoutines] = useState({}); // 루틴 목록을 저장합니다.
+//   const [newRoutineName, setNewRoutineName] = useState(""); // 새 루틴 이름 입력을 위한 상태입니다.
+//   const [routineDays, setRoutineDays] = useState({ // 루틴의 요일 선택 상태입니다.
+//     mon: false, tue: false, wed: false, thu: false, fri: false, sat: false, sun: false, everyday: false
+//   });
 
-//   // TODO: 루틴 추가 관련 상태 관리 코드를 작성하세요.
-//   // const [newRoutineName, setNewRoutineName] = useState("");
-//   // ...
+//   // 현재 주의 시작 날짜를 키 값으로 사용합니다.
+//   const currentWeekKey = currentWeek[0]?.format('YYYY-MM-DD');
 
-//   // TODO: 루틴 추가 및 상태 변경 관련 함수를 작성하세요.
-//   // const addRoutine = () => { ... };
-//   // ...
+//   // 새 루틴을 추가하는 함수입니다.
+//   const addRoutine = () => {
+//     // TODO: 새 루틴을 생성하고 routines 상태를 업데이트하는 로직을 작성하세요.
+//     // 1. 새 루틴 객체를 생성합니다.
+//     // 2. setRoutines 함수를 사용하여 routines 상태에 새 루틴을 추가합니다.
+//   };
+
+//   // 요일 선택 상태를 초기화하는 함수입니다.
+//   const resetRoutineDays = () => {
+//     setRoutineDays({
+//       mon: false, tue: false, wed: false, thu: false, fri: false, sat: false, sun: false, everyday: false
+//     });
+//   };
+
+//    // 루틴의 요일 선택을 변경하는 함수입니다.
+//    const handleCheckboxChange = (day) => {
+//     if (day === "everyday") {
+//       const newEverydayValue = !routineDays.everyday;
+//       setRoutineDays({
+//         mon: newEverydayValue, tue: newEverydayValue, wed: newEverydayValue, thu: newEverydayValue,
+//         fri: newEverydayValue, sat: newEverydayValue, sun: newEverydayValue, everyday: newEverydayValue
+//       });
+//     } else {
+//       setRoutineDays({ ...routineDays, [day]: !routineDays[day], everyday: false });
+//     }
+//   };
+
+//   // 루틴의 상태(진행 상태)를 변경하는 함수입니다.
+//   const handleRoutineStatusChange = (routineId, day, status) => {
+//     // TODO: 특정 루틴의 상태를 업데이트하는 로직을 작성하세요.
+//     // 1. routines 상태에서 해당 루틴을 찾습니다.
+//     // 2. 선택된 요일의 상태를 업데이트합니다.
+//     // 3. setRoutines 함수로 전체 routines 상태를 업데이트합니다.
+//   };
+
+//   // 루틴의 이름을 수정하는 함수입니다.
+//   const handleRoutineEdit = (routineId, newName) => {
+//     setRoutines(prevRoutines => {
+//       const weekRoutines = prevRoutines[currentWeekKey] || [];
+//       const updatedWeekRoutines = weekRoutines.map(routine => {
+//         if (routine.id === routineId) {
+//           return { ...routine, name: newName };
+//         }
+//         return routine;
+//       });
+
+//       return { ...prevRoutines, [currentWeekKey]: updatedWeekRoutines };
+//     });
+//   };
 
 //   return (
-//     <div>
+//     <div className="routine-manager">
 //       {/* Calendar 컴포넌트를 렌더링합니다. */}
 //       <Calendar
 //         currentWeek={currentWeek}
 //         routines={routines}
-//         // TODO: 필요한 props를 전달하세요.
+//         onSelectDay={(day) => console.log(day)}
+//         onNextWeek={nextWeek}
+//         onPreviousWeek={previousWeek}
+//         onRoutineStatusChange={handleRoutineStatusChange}
+//         onRoutineEdit={handleRoutineEdit}
 //       />
 
 //       {/* 루틴 추가 폼을 렌더링합니다. */}
-//       <div>
-//         {/* TODO: 루틴 추가 폼 관련 코드를 작성하세요. */}
+//       <div className="routine-add-form">
+//         <input
+//           type="text"
+//           value={newRoutineName}
+//           onChange={(e) => setNewRoutineName(e.target.value)}
+//           placeholder="새 루틴 이름"
+//         />
+//         // 루틴의 요일 선택 체크박스를 렌더링합니다.
+//         {Object.keys(routineDays).map(day => (
+//           <label key={day}>
+//           {/* TODO: 각 요일에 대한 체크박스를 구현하세요. */}
+//           {/* 체크박스가 변경될 때, handleCheckboxChange 함수가 호출되도록 합니다. */}
+//           {/* 체크 상태는 routineDays 상태에서 관리됩니다. */}
+//           </label>
+//         ))}
+//         <button onClick={addRoutine}>루틴 추가</button>
 //       </div>
 //     </div>
 //   );
